475c6ee22af3a57f7381e1f7b5b335bfa6593c25
MasterVVK <vvk@2140101.ru>
2024-07-08 13:50:14 +0500

    Create cat_api_handler.py

---
+++ cat_api_handler.py
@@ -0,0 +1,26 @@
+# cat_api_handler.py
+
+import requests
+from aiogram import Router, F
+from aiogram.types import Message
+import json
+from aiogram.filters import Command
+
+router = Router()
+
+# Загрузка конфигурации из файла config.json
+with open('config.json', 'r', encoding='utf-8') as config_file:
+    config = json.load(config_file)
+
+CAT_API_KEY = config['CAT_API_KEY']
+
+@router.message(Command("cat"))
+async def send_cat_image(message: Message):
+    url = f'https://api.thecatapi.com/v1/images/search?api_key={CAT_API_KEY}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        cat_image_url = data[0]['url']
+        await message.reply_photo(photo=cat_image_url)
+    else:
+        await message.reply("Не удалось получить изображение кота. Попробуйте позже.")
