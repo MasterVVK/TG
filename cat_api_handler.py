fe3a9718badcd8985f197edfc2a8b378ca3dca6e
MasterVVK <vvk@2140101.ru>
2024-07-08 20:49:42 +0500

    Update cat_api_handler.py

--- cat_api_handler.py
+++ cat_api_handler.py
@@ -17,13 +17,21 @@ CAT_API_KEY = config['CAT_API_KEY']
 
 @router.message(Command("cat"))
 async def send_cat_image(message: Message):
-    url = f'https://api.thecatapi.com/v1/images/search?api_key={CAT_API_KEY}'
+    url = f'https://api.thecatapi.com/v1/images/search?api_key={CAT_API_KEY}&include_breeds=true'
     response = requests.get(url)
     logging.debug(f"Запрос к TheCatAPI: {response.url}")
     if response.status_code == 200:
         data = response.json()
         logging.debug(f"Ответ от TheCatAPI: {data}")
         cat_image_url = data[0]['url']
-        await message.reply_photo(photo=cat_image_url)
+        breeds = data[0].get('breeds', [])
+        if breeds:
+            breed_info = breeds[0]
+            breed_name = breed_info['name']
+            breed_description = breed_info['description']
+            caption = f"Порода: {breed_name}\n\nОписание: {breed_description}"
+        else:
+            caption = "Не удалось определить породу этой кошки."
+        await message.reply_photo(photo=cat_image_url, caption=caption)
     else:
         await message.reply("Не удалось получить изображение кота. Попробуйте позже.")
