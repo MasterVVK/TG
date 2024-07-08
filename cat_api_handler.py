56891e4549fecd678588a7fdf79c7ede9bdac953
MasterVVK <vvk@2140101.ru>
2024-07-08 20:41:58 +0500

    update cat_api

--- cat_api_handler.py
+++ cat_api_handler.py
@@ -5,6 +5,7 @@ from aiogram import Router, F
 from aiogram.types import Message
 import json
 from aiogram.filters import Command
+import logging
 
 router = Router()
 
@@ -18,8 +19,10 @@ CAT_API_KEY = config['CAT_API_KEY']
 async def send_cat_image(message: Message):
     url = f'https://api.thecatapi.com/v1/images/search?api_key={CAT_API_KEY}'
     response = requests.get(url)
+    logging.debug(f"Запрос к TheCatAPI: {response.url}")
     if response.status_code == 200:
         data = response.json()
+        logging.debug(f"Ответ от TheCatAPI: {data}")
         cat_image_url = data[0]['url']
         await message.reply_photo(photo=cat_image_url)
     else:
--- nasa_api_handler.py
+++ nasa_api_handler.py
@@ -1,9 +1,12 @@
 # nasa_api_handler.py
+
 import requests
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 import json
+import logging
+from googletrans import Translator
 
 router = Router()
 
@@ -13,21 +16,27 @@ with open('config.json', 'r', encoding='utf-8') as config_file:
 
 NASA_API_KEY = config['NASA_API_KEY']
 
+translator = Translator()
 
 @router.message(Command("nasa"))
 async def send_nasa_apod(message: Message):
     url = f'https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}'
     response = requests.get(url)
+    logging.debug(f"Запрос к NasaAPI: {response.url}")
     if response.status_code == 200:
         data = response.json()
         apod_url = data['url']
         title = data['title']
         explanation = data['explanation']
 
-        await message.reply_photo(photo=apod_url, caption=title)
+        # Перевод названия и объяснения на русский язык
+        title_ru = translator.translate(title, dest='ru').text
+        explanation_ru = translator.translate(explanation, dest='ru').text
+
+        await message.reply_photo(photo=apod_url, caption=title_ru)
 
         # Разбиваем описание на части, если оно слишком длинное
-        parts = [explanation[i:i + 1024] for i in range(0, len(explanation), 1024)]
+        parts = [explanation_ru[i:i + 1024] for i in range(0, len(explanation_ru), 1024)]
         for part in parts:
             await message.reply(part)
     else:
